befd2812bfd69ae984aef220d3f14782797e00d8
mohsn <mohsn.mohsni278@gmail.com>
2020-12-17 17:53:59 +0330

    coplete account models that contain all model related to account app

--- app_account/models.py
+++ app_account/models.py
@@ -5,21 +5,21 @@ from django.contrib.auth.models import Permission
 
 
 class UserManager(BaseUserManager):
-    def _create_user(self, email, name, password, **extra_fields):
+    def _create_user(self, email, full_name, password, **extra_fields):
         if not email:
             raise ValueError('The given username must be set')
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name, **extra_fields)
+        user = self.model(email=email, full_name=full_name, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, email, name=None, password=None, **extra_fields):
+    def create_user(self, email, full_name=None, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, password, name, **extra_fields)
+        return self._create_user(email, full_name, password, **extra_fields)
 
-    def create_superuser(self, email, name=None, password=None, **extra_fields):
+    def create_superuser(self, email, full_name=None, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
@@ -28,20 +28,19 @@ class UserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
-        return self._create_user(email, name, password, **extra_fields)
+        return self._create_user(email, full_name, password, **extra_fields)
 
 
 class User(AbstractBaseUser, Permission):
-    email = models.EmailField(_('Email'), blank=False, db_index=True,
+    email = models.EmailField(_('Email'), blank=False, db_index=True, unique=True,
                               help_text=_('required valid email like example@gamil.com'))
-    name = models.CharField(_('Name'), blank=True, max_length=100,
-                            help_text=_('enter your full name'))
-    avatar = models.ImageField(_('Avatar'), blank=True, upload_to='user/images')
-    is_staff = models.BooleanField(_('staff status'), default=False,
+    full_name = models.CharField(_('Name'), blank=True, max_length=100,
+                                 help_text=_('enter your full name'))
+    is_staff = models.BooleanField(_('Staff status'), default=False,
                                    help_text=_(
                                        'Designates whether the user can log into this admin site.'
                                    ))
-    is_active = models.BooleanField(_('active'), default=True,
+    is_active = models.BooleanField(_('Active'), default=True,
                                     help_text=_(
                                         'Designates whether this user should be treated as active.'
                                         'Unselect this instead of deleting accounts.'
@@ -59,3 +58,75 @@ class User(AbstractBaseUser, Permission):
     def clean(self):
         super().clean()
         self.email = self.__class__.objects.normalize_email(self.email)
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_('User'),
+                             related_name='profile', related_query_name='profile')
+    phone = models.IntegerField(_('Phone'), blank=True, default=0)
+    avatar = models.ImageField(_('Avatar'), blank=True, upload_to='user/images')
+    is_seller = models.BooleanField(_('User status'), blank=True, default=False)
+
+    class Meta:
+        verbose_name = _('Profile')
+
+
+class Shop(models.Model):
+    name = models.CharField(_('Shop'), max_length=100)
+    slug = models.SlugField(_('Slug'), unique=True)
+    description = models.TextField(_('Description'))
+    image = models.ImageField(_('Picture'), upload_to='shop/images', blank=True)
+
+    class Meta:
+        verbose_name = _('Shop')
+        verbose_name_plural = _('Shops')
+
+
+class Email(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_('User'),
+                             related_name='email_sent', related_query_name='email_sent')
+    subject = models.CharField(_('Subject'), max_length=120)
+    to = models.EmailField(_('To'))
+    body = models.TextField(_('Text body'))
+
+    class Meta:
+        verbose_name = _('Email')
+        verbose_name_plural = _('Emails')
+
+    def get_email(self):
+        return str(self.to)
+
+
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_('User'),
+                             related_name='address', related_query_name='address')
+    city = models.CharField(_('City'), max_length=100)
+    street = models.CharField(_('Street'), max_length=100)
+    alley = models.CharField(_('Alley'), max_length=50)
+    no = models.IntegerField(_('Home no'))
+    postal_code = models.IntegerField(_('Postal Code'))
+
+    class Meta:
+        verbose_name = _('Address')
+        verbose_name_plural = _('Addresses')
+
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_('User'),
+                             related_name='notification', related_query_name='notification')
+    subject = models.CharField(_('Subject'), max_length=100)
+    body = models.TextField(_('Text body'))
+
+    class Meta:
+        verbose_name = _('Notification')
+        verbose_name_plural = _('Notifications')
+
+
+# class Like(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name=_('User'),
+#                                 related_name='like', related_query_name='like')
+#     products = models.ManyToManyField('Product', verbose_name=_('Products liked'),
+#                                       related_name='products', related_query_name='products')
+#
+#     class Meta:
+#         verbose_name = _('Like')
--- core/settings.py
+++ core/settings.py
@@ -123,7 +123,7 @@ AUTH_USER_MODEL = 'app_account.User'
 
 # Static Config
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'asset'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Media Config
