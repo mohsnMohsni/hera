57d094e777d409351169a6266103c7b094177bb8
mohsn <sir.mohsn@gmail.com>
2021-01-17 15:32:16 +0330

    make responsive pages

--- app_product/views.py
+++ app_product/views.py
@@ -8,7 +8,9 @@ class CategoryDetail(DetailView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data()
-        products = Product.objects.prefetch_related('shop_product').filter(category=context.get('category'))[:8]
+        products = Product.objects.prefetch_related(
+            'shop_product'
+        ).filter(category=context.get('category'))[:8]
         context['products'] = products
         return context
 
@@ -16,3 +18,9 @@ class CategoryDetail(DetailView):
 class ProductDetail(DetailView):
     model = Product
     template_name = 'main/product.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['product1'] = Product.objects.all().values_list('id', flat=True)
+        print(context['product1'])
+        return context
