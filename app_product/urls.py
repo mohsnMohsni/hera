c2f9dad0116f79389225a07e9b5b3dc4c6873972
mohsn <sir.mohsn@gmail.com>
2021-01-18 09:16:05 +0330

    add search feature

--- app_product/urls.py
+++ app_product/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CategoryDetail, ProductDetail
+from .views import CategoryDetail, ProductDetail, SearchView
 
 app_name = 'product'
 
 urlpatterns = [
     path('category/<slug:slug>/', CategoryDetail.as_view(), name='category'),
     path('product/<slug:slug>/', ProductDetail.as_view(), name='product'),
+    path('search/', SearchView.as_view(), name='search'),
 ]
--- app_product/views.py
+++ app_product/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import DetailView
+from django.db.models import Q
+from django.views.generic import DetailView, ListView
 from .models import Category, Product
 
 
@@ -23,4 +24,22 @@ class ProductDetail(DetailView):
         context = super().get_context_data()
         context['product1'] = Product.objects.all().values_list('id', flat=True)
         print(context['product1'])
+        return context
+
+
+class SearchView(ListView):
+    model = Product
+    template_name = 'main/search.html'
+    paginate_by = 8
+
+    def get_queryset(self):
+        query = self.request.GET.get('search')
+        object_list = Product.objects.filter(
+            Q(name__contains=query) | Q(slug__contains=query)
+        )
+        return object_list
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        context['search_key'] = self.request.GET.get('search')
         return context
